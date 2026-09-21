32751c656e4b19bc2e19d494b07c3958a1d391cb
Erald Hoxha <erald.hoxha@elev.ga.ntig.se>
2026-09-21 09:40:32 +0200

    quiz

--- Quiz/obj/Debug/net10.0/Quiz.AssemblyInfo.cs
+++ Quiz/obj/Debug/net10.0/Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511cd6ced2854340e15ebfae1f433d20c5273da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
